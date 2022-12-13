733ee1cb321dbea2a53b75ce2efc1e07df38d518
OM CHAUHAN <e22cseu1437@bennett.edu.in>
2022-12-13 15:40:23 +0530

    Update Main.py
    This is only the initial UI part.

--- Password Manager/Main.py
+++ Password Manager/Main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button, Entry
+from tkinter import CENTER, Tk, Label, Button, Entry, Frame
 from tkinter import  ttk
 
 
@@ -10,17 +10,51 @@ class root_window:
         self.root.geometry("900x600+40+40")
 
         head_title = Label(self.root, text='Password Manager',width=40,
-        bg="purple", font=("Ariel",20),padx=10,pady=10, justify="center",anchor="center").grid(columnspan=4, padx=140,pady=10)
+        bg="purple", font=("Ariel",20),padx=10,pady=10, justify=CENTER,
+        anchor="center").grid(columnspan=4, padx=140,pady=20)
 
-        crud_frame=ttk.Frame(self.root)
-        crud_frame.grid()
+        self.crud_frame = Frame(self.root, highlightbackground="black",
+        highlightthickness=1, padx=10, pady=30)
+        self.crud_frame.grid()
+        self.create_entry_labels()
+        self.create_entry_boxes()
+        self.create_crud_buttons()
 
-        entry_boxes = []
-        row_no = col_no = 0
+    def create_entry_labels(self):
+        self.col_no, self.row_no = 0, 0
+        labels_info = ('ID', 'Website', 'Username', 'Password')
+        for label_info in labels_info:
+            Label(self.crud_frame, text=label_info, bg='grey',
+            fg='white', font=("Ariel",12), padx=5, pady=2).grid(row=self.row_no,
+            column=self.col_no,padx=5, pady=2)
+            self.col_no+=1
+
+    def create_crud_buttons(self):
+        self.row_no+=1
+        self.col_no = 0
+        buttons_info = (('Save', 'green'), ('Update', 'blue'),
+        ('Delete', 'red'), ('Copy Password', 'violet'))
+        for btn_info in buttons_info:
+            Button(self.crud_frame, text=btn_info[0], bg=btn_info[1],
+            fg='white', font=("Ariel",12), padx=2, pady=1, width=20).grid(row=self.row_no,
+            column=self.col_no,padx=5, pady=10)
+            self.col_no+=1
+
+
+        
+    def create_entry_boxes(self):
+        self.row_no+=1
+        self.entry_boxes = []
+        self.col_no = 0
         for i in range(4):
-            entry_box = Entry(crud_frame, width=30, background='lightgrey')
-            entry_box.grid(row=row_no, column=col_no,padx=5,pady=2)
-            col_no+=1
+            show=""
+            if i == 3 :
+                show = "*"
+            entry_box = Entry(self.crud_frame, width=20, background='lightgrey',
+            font = ("Ariel", 12), show = show)
+            entry_box.grid(row=self.row_no, column=self.col_no,padx=5,pady=2)
+            self.col_no+=1
+            self.entry_boxes.append(entry_box)
 
 
 
